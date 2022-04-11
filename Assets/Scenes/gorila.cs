6e7a6d6f5dda8684a811c517a0994739168e7d01
joacoorqueda <101809787+joacoorqueda@users.noreply.github.com>
2022-04-11 10:38:20 -0300

    quandale pizza

--- Assets/Scenes/gorila.cs
+++ Assets/Scenes/gorila.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class gorila : MonoBehaviour
 {
+    public string nombre;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(nombre + "le gusta la pizza");   
     }
 
     // Update is called once per frame
